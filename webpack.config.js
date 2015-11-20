a560115e6534fc5f13e0a23f4a18a93627090fb8
wengqi <qiweng@wisedu.com>
2015-11-20 08:50:03 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
+
+var entry = {};
+entry["bh-grid"] = ["./src/index.js"];
+
+module.exports = {
+  entry: entry,
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
+  output: {
+    path: path.join(process.cwd(), 'dist'),
+    filename: '[name].js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query:{
+          "compact": false,
+          "presets": ["react", "es2015", "stage-0", "stage-1", "stage-2", "stage-3"]
+        }
+      }, 
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }, 
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract(
+            'style-loader',"css-loader!less-loader"
+        )
+      }, 
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(
+            'style-loader', 'css-loader'
+        )
+      },
+      {test: /\.scss$/, loader: 'style!css!sass?sourceMap'}
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+
+  devtool: 'source-map'
+};
